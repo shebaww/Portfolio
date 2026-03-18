d5d6290d743378ae8bb05fa1d0055bd49ccb4684
shebaww <nahomnatnael87@gmail.com>
2026-03-19 00:10:08 +0300

    Clean up built files from main branch

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
+    base: '/Portfolio/',
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
